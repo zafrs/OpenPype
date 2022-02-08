7253d396928a1a07c385e5f44c89b5e117e2aeec
zafrs <trr.faris@gmail.com>
2022-02-08 18:11:50 +0700

    add "render_mov_batch" (hardcode)

--- openpype/plugins/publish/integrate_new.py
+++ openpype/plugins/publish/integrate_new.py
@@ -102,7 +102,8 @@ class IntegrateAssetNew(pyblish.api.InstancePlugin):
                 "xgen",
                 "hda",
                 "reference",
-                "texture_batch"
+                "texture_batch",
+                "render_mov_batch"
                 ]
     exclude_families = ["clip"]
     db_representation_context_keys = [
--- openpype/version.py
+++ openpype/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring Pype version."""
-__version__ = "3.8.1-nightly.1v2022-02-8.1"
+__version__ = "3.8.2-Lumine220209"
